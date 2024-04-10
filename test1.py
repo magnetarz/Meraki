dd41e9c946bad7948340d842d158d083d7b120a9
magnetarz <kwardrip105@gmail.com>
2024-04-09 23:39:10 -0700

    updated test1 with working functions test2 created putting network id in topology and wan stats

--- test1.py
+++ test1.py
@@ -4,6 +4,7 @@ import json
 import os
 import sys
 import urllib.parse
+from pprint import pprint
 from datetime import datetime
 from key import MERAKI_API_KEY
 
@@ -18,14 +19,19 @@ def get_org_id(org):
     org_id = orgs[0]['id']
     return org_id
 
-def get_network_id(network):
-    networks = dashboard.organizations.getOrganizationNetworks(org_id, name="CCNP")
+def get_network_id(network, org_id):
+    networks = dashboard.organizations.getOrganizationNetworks(org_id, name=network)
     network_id = networks[0]['id']
     return network_id
 
-def main(org_id, network_id):
-    topology = dashboard.networks.getNetworkTopologyLinkLayer(network_id)
-    wan_stats = dashboard.appliance.getOrganizationApplianceUplinkStatuses(org_id, network_id)
+def main(org_name, network_name):
+    org_id = get_org_id(org_name)
+    network_id = get_network_id(network_name, org_id)
+
+    topology = dashboard.networks.getNetworkTopologyLinkLayer(networkId=network_id)
+    pprint(topology)
+    wan_stats = dashboard.appliance.getOrganizationApplianceUplinkStatuses(organizationId=org_id, networkId=network_id)
+    pprint(wan_stats)
     
        
 
@@ -34,7 +40,7 @@ if __name__ == '__main__':
     parser.add_argument('-org', help='Organization name to get the network from.')
     parser.add_argument('-network', help='Network you want the topology of.')
     args = parser.parse_args()
-    print("About to gather network: " + args.network_name + "'s topology.")
+    print("About to gather network: " + args.network + "'s topology.")
     
 
     start_time = datetime.now()
--- test2.py
+++ test2.py
@@ -3,6 +3,7 @@ import meraki
 import json
 import os
 import urllib.parse
+from pprint import pprint
 from datetime import datetime
 from key import MERAKI_API_KEY
 
@@ -11,14 +12,20 @@ dashboard = meraki.DashboardAPI(
     output_log=False
     )
 
+
+parser = argparse.ArgumentParser(description='Generate a CSV file of current topology for a specific network.')
+parser.add_argument('-org', help='Organization name to get the network from.')
+parser.add_argument('-network', help='Network you want the topology of.')
+args = parser.parse_args()
 orgs = dashboard.organizations.getOrganizations("Devnet Sandbox")
 org_id = orgs[0]['id']
 
 
 networks = dashboard.organizations.getOrganizationNetworks(org_id, name="CCNP")
 network_id = networks[0]['id']
+print(network_id)
 
 
 topology = dashboard.networks.getNetworkTopologyLinkLayer(network_id)
 wan_stats = dashboard.appliance.getOrganizationApplianceUplinkStatuses(org_id, network_id)
-print(wan_stats)
+pprint(wan_stats[3]['uplinks'])
